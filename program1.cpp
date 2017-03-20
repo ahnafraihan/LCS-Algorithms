f9a5e40dbe855c12d04b19cf773dba6f7e9fc0fa
ahnafraihan <araihan1@binghamton.edu>
2017-03-20 12:37:24 -0400

    Update program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -1,5 +1,5 @@
 //Ahnaf Raihan
-//CS375 - program1
+//program1.cpp
 
 /* program1.cpp contains methods: int max, int lcs, and int main. program1.cpp serves to demonstrate the use of bottom-up dynamic programming in order to solve the LCS problem. */
 
